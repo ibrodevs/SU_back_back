70958b8e393977b608bdfb8655d2fa066375e271
Ibro <ibroabdraimov20@gmail.com>
2026-02-05 15:54:30 +0600

    commit

--- back_su_m/settings.py
+++ back_su_m/settings.py
@@ -129,31 +129,36 @@ MODELTRANSLATION_PREPOPULATE_LANGUAGE = 'ru'
 # -------------------
 # AWS S3 Storage
 # -------------------
-AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
-AWS_QUERYSTRING_AUTH = False  # файлы будут открываться без подписей
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", default=None)
 
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
+if AWS_ACCESS_KEY_ID:
+    AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_REGION_NAME = config("AWS_S3_REGION_NAME")
+    AWS_QUERYSTRING_AUTH = False  # файлы будут открываться без подписей
 
-STORAGES = {
-    "default": {  # медиа
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-        "OPTIONS": {
-            "location": "media",
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
+
+    STORAGES = {
+        "default": {  # медиа
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+            "OPTIONS": {
+                "location": "media",
+            },
         },
-    },
-    "staticfiles": {  # статика
-        "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
-        "OPTIONS": {
-            "location": "static",
+        "staticfiles": {  # статика
+            "BACKEND": "storages.backends.s3boto3.S3StaticStorage",
+            "OPTIONS": {
+                "location": "static",
+            },
         },
-    },
-}
+    }
 
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
-STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+else:
+    STATIC_URL = '/static/'
+    MEDIA_URL = '/media/'
 
 # -------------------
 # Django REST Framework
